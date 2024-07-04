533b2fe071e34b521269b1eea47ba5d07413fa98
Muhammad Hibatur Akmal <110396432+MHAkmal@users.noreply.github.com>
2024-07-04 16:45:18 +0700

    Add files via upload

---
+++ dbt_project_1/models/example/my_first_dbt_model.sql
@@ -0,0 +1,26 @@
+create or replace table `my-data-engineer-project`.`dbt_project`.`my_first_dbt_model`
+      
+    
+    
+
+    OPTIONS()
+    as (
+      create or replace table `my-data-engineer-project`.`dbt_project`.`my_first_dbt_model`
+      
+    
+    
+
+    OPTIONS()
+    as (
+      with source_data as (
+
+    select 1 as id
+    union all
+    select null as id
+
+)
+
+select *
+from source_data
+    );
+    );
---
+++ dbt_project_1/models/example/my_second_dbt_model.sql
@@ -0,0 +1,19 @@
+create or replace table `my-data-engineer-project`.`dbt_project`.`my_second_dbt_model`
+      
+    
+    
+
+    OPTIONS()
+    as (
+      create or replace table `my-data-engineer-project`.`dbt_project`.`my_second_dbt_model`
+      
+    
+    
+
+    OPTIONS()
+    as (
+      select * 
+from `my-data-engineer-project`.`dbt_project`.`my_first_dbt_model`
+where id = 1
+    );
+    );
